8971aad06adc3d1487eb0be4122778dee87f3791
Mario Vallejo <MarioGV320@hotmail.com>
2021-08-30 17:33:23 -0500

    Lexico completo

--- Lexico/Lexico.py
+++ Lexico/Lexico.py
@@ -18,6 +18,58 @@ for caracter in fuente:
         elif caracter==" ":
             estado=0
 
+        elif caracter=="+" or caracter=="-":
+            simbolo+=caracter
+            estado=4
+
+        elif caracter=="*" or caracter=="/":
+            simbolo+=caracter
+            estado=5
+
+        elif caracter=="<" or caracter==">":
+            simbolo+=caracter
+            estado=6
+        
+        elif caracter=="|":
+            simbolo+=caracter
+            estado=8
+
+        elif caracter=="&":
+            simbolo+=caracter
+            estado=10
+
+        elif caracter=="!":
+            simbolo+=caracter
+            estado=12
+
+        elif caracter=="=":
+            simbolo+=caracter
+            estado=14
+
+        elif caracter==";":
+            simbolo+=caracter
+            estado=15
+
+        elif caracter==",":
+            simbolo+=caracter
+            estado=16
+
+        elif caracter=="(":
+            simbolo+=caracter
+            estado=17
+
+        elif caracter==")":
+            simbolo+=caracter
+            estado=18
+
+        elif caracter=="{":
+            simbolo+=caracter
+            estado=19
+
+        elif caracter=="}":
+            simbolo+=caracter
+            estado=20
+
         else: #Error
             simbolo+=caracter
             estado=404
@@ -32,8 +84,21 @@ for caracter in fuente:
 
         elif caracter == " ": #Aceptacion
             estado=0
-            simbolos[simbolo] = "Identificador"
-            simbolo= ""
+            if simbolo=="int" or simbolo=="float" or simbolo=="void":
+                simbolos[simbolo] = "Tipo"
+
+            elif simbolo=="if":
+                simbolos[simbolo] = "if"
+
+            elif simbolo=="while":
+                simbolos[simbolo] = "while"
+
+            elif simbolo=="return":
+                simbolos[simbolo] = "return"
+
+            else:
+                simbolos[simbolo] = "Identificador"
+                simbolo= ""
         
         else: #Error
             simbolo+=caracter
@@ -49,7 +114,7 @@ for caracter in fuente:
 
         elif caracter == " ": #Aceptacion
             estado=0
-            simbolos[simbolo] = "Real"
+            simbolos[simbolo] = "Entero"
             simbolo= ""
 
         else: #Error
@@ -67,6 +132,203 @@ for caracter in fuente:
             simbolo= ""
 
         else: #Error
+            simbolo+=caracter
+            estado=404
+    
+    elif estado==4:
+
+        if caracter == " ":
+            estado=0
+            simbolos[simbolo] = "OpSuma"
+            simbolo= ""
+
+        else: 
+            simbolo+=caracter
+            estado=404
+
+    elif estado==5:
+
+        if caracter == " ":
+            estado=0
+            simbolos[simbolo] = "OpMul"
+            simbolo= ""
+
+        else: 
+            simbolo+=caracter
+            estado=404
+    
+    elif estado==6:
+
+        if caracter == " ":
+            estado=0
+            simbolos[simbolo] = "OpRelac"
+            simbolo= ""
+        
+        elif caracter == "=":
+            simbolo+=caracter
+            estado=7
+
+        else: 
+            simbolo+=caracter
+            estado=404
+
+    elif estado==7:
+
+        if caracter == " ":
+            estado=0
+            simbolos=[simbolo] = "OpRelac"
+            simbolo= ""
+        
+        else:
+            simbolo+=caracter
+            estado=404
+    
+    elif estado==8:
+
+        if caracter=="|":
+            estado=9
+            simbolo+=caracter
+
+        else:
+            simbolo+=caracter
+            estado=404
+
+    elif estado==9:
+
+        if caracter==" ":
+            estado=0
+            simbolos[simbolo] = "OpOr"
+            simbolo= ""
+
+        else:
+            simbolo+=caracter
+            estado=404
+
+    elif estado==10:
+
+        if caracter=="&":
+            estado=11
+            simbolo+=caracter
+
+        else:
+            simbolo+=caracter
+            estado=404
+
+    elif estado==11:
+
+        if caracter==" ":
+            estado=0
+            simbolos[simbolo] = "OpAnd"
+            simbolo= ""
+
+        else:
+            simbolo+=caracter
+            estado=404
+    
+    elif estado==12:
+
+        if caracter==" ":
+            estado=0
+            simbolos[simbolo]="OpNot"
+            simbolo= ""
+        
+        elif caracter=="=":
+            estado=13
+            simbolo+=caracter
+
+        else:
+            simbolo+=caracter
+            estado=404
+
+    elif estado==13:
+
+        if caracter==" ":
+            estado=0
+            simbolos[simbolo]="OpIgualdad"
+            simbolo=""
+
+        else:
+            simbolo+=caracter
+            estado=404
+
+    elif estado==14:
+
+        if caracter==" ":
+            estado=0
+            simbolos[simbolo]="="
+            simbolo=""
+
+        elif caracter=="=":
+            estado=13
+            simbolo+=caracter
+        
+        else:
+            simbolo+=caracter
+            estado=404
+    
+    elif estado==15:
+
+        if caracter==" ":
+            estado=0
+            simbolos[simbolo]=";"
+            simbolo=""
+
+        else:
+            simbolo+=caracter
+            estado=404
+
+    elif estado==16:
+
+        if caracter==" ":
+            estado=0
+            simbolos[simbolo]=","
+            simbolo=""
+
+        else:
+            simbolo+=caracter
+            estado=404
+    
+    elif estado==17:
+
+        if caracter==" ":
+            estado=0
+            simbolos[simbolo]="("
+            simbolo=""
+
+        else:
+            simbolo+=caracter
+            estado=404            
+
+    elif estado==18:
+
+        if caracter==" ":
+            estado=0
+            simbolos[simbolo]=")"
+            simbolo=""
+
+        else:
+            simbolo+=caracter
+            estado=404
+
+    elif estado==19:
+
+        if caracter==" ":
+            estado=0
+            simbolos[simbolo]="{"
+            simbolo=""
+
+        else:
+            simbolo+=caracter
+            estado=404
+
+    elif estado==20:
+
+        if caracter==" ":
+            estado=0
+            simbolos[simbolo]="}"
+            simbolo=""
+
+        else:
             simbolo+=caracter
             estado=404
 
